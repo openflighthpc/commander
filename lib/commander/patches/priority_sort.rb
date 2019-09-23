3fe2ce88abce930bec85f66ae16e720c2e974d64
william <william.mccumstie@gmail.com>
2019-09-23 12:12:41 +0100

    Allow the order to be set by priority
    This will override the default priority of each command. This allows
    them to be sorted non alphabetically

--- lib/commander/patches/priority_sort.rb
+++ lib/commander/patches/priority_sort.rb
@@ -3,13 +3,18 @@
 module Commander
   module Patches
     module PrioritySort
+      attr_accessor :priority
+
       def <=>(other)
         # Different classes can not be compared and thus are considered
         # equal in priority
         return 0 unless self.class == other.class
 
-        # Delegate the sort based on the name
-        self.name <=> other.name
+        # Sort firstly based on the commands priority
+        comp = (self.priority || 0) <=> (other.priority || 0)
+
+        # Fall back on name comparison if priority is equal
+        comp == 0 ? self.name <=> other.name : comp
       end
     end
   end
